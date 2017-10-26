38c952b8a321a8f8d053097e355af983d5d0b7da
Ezekiel <ezekielaquino@users.noreply.github.com>
2017-10-26 11:13:07 +0200

    🍣 Initial Commit

---
+++ Marquee3G.js
@@ -0,0 +1,157 @@
+// A WORK IN PROGRESS
+class Marquee {
+  constructor(options) {
+    // ES6 this later
+    this.element = options.element;
+    this.gsap = options.gsap;
+    this.originalContent = this.element.children[0];
+    this.destroy = this.destroy.bind(this);
+
+    this.setInstanceBounds();
+    this.calculateRepetition();
+    this.createWrapper();
+    this.cloneNodes();
+    this.animate();
+    this.handleResize();
+  }
+
+
+  // Rename vars---- too long
+  setInstanceBounds() {
+    // make sure we calculate the non-block bounding box
+    this.originalContent.style.display = `inline-block`;
+
+    this.bounds = {
+      element: this.element.getBoundingClientRect(),
+      content: this.element.querySelector('.marquee-container').getBoundingClientRect()
+    };
+  }
+
+
+  calculateRepetition() {
+    const repetitions = (this.bounds.element.width + this.bounds.content.width) / this.bounds.content.width;
+    this.repetitions = Math.ceil(repetitions);
+    console.log(`Required repetitions: ${this.repetitions}`);
+  }
+
+
+  getChildren() {
+    if (this.element.children.length > 1) {
+      throw 'There is more than one child. Please only have one wrapper with `.marquee-container` selector.'
+    } else {
+      const elem = this.element.children[0];
+      elem.classList.add('marquee-copy');
+      return elem;
+    }
+  }
+
+
+  createWrapper() {
+    const wrapper  = document.createElement('div');
+    wrapper.classList.add('marquee-wrapper');
+    wrapper.style.whiteSpace = 'nowrap';
+    this.element.appendChild(wrapper);
+    this.element.style.overflow = 'hidden';
+    this.wrapper = wrapper;
+  }
+
+
+  cloneNodes(amount) {
+    [...Array(amount || this.repetitions)].map((val, index) => {
+      const clone = this.originalContent.cloneNode(true);
+      this.wrapper.appendChild(clone);
+      return clone;
+    });
+
+    if (!amount) this.originalContent.remove();
+  }
+
+
+  animate() {
+    if (this.timeline) {
+      this.timeline.kill();
+    }
+
+    const tl = new this.gsap({
+      onComplete: () => { tl.restart(); }
+    });
+
+    tl.set(this.wrapper, { x: 0 });
+    tl.to(this.wrapper, 2, {
+      x: this.bounds.content.width * -1,
+      force3D: true,
+      ease: Power0.easeNone
+    });
+
+    this.timeline = tl;
+  }
+
+
+  refresh() {
+    const prevRepetitions = this.repetitions;
+
+    this.calculateRepetition();
+
+    const diff = this.repetitions - prevRepetitions;
+    if (diff > 0) this.cloneNodes(diff);
+
+    this.setInstanceBounds();
+    this.animate();
+  }
+
+  
+  handleResize() {
+    window.addEventListener('resize', debounce(() => {
+      this.refresh();
+    }, 50));
+  }
+
+
+  destroy() {
+    this.timeline.kill();
+    this.element.children[0].remove();
+    this.element.append(this.originalContent);
+    delete window.marquees[this.name || this.index];
+  }
+
+
+  static init(options) {
+    window.Marquees = {};
+    const elems = document.querySelectorAll(options.selector || '.marquee');
+
+    _init();
+
+    function _init() {
+      for (let i = 0; i < elems.length; i++) {
+        const marquee = elems[i];
+        const instance = new Marquee({
+          element: marquee,
+          gsap: options.gsap
+        });
+
+        instance.index = i;
+        window.Marquees[i] = instance;
+      }
+    }
+  }
+}
+
+
+function debounce(func, wait, immediate) {
+  let timeout;
+  return function() {
+    const context = this;
+    const args = arguments;
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+
+export default Marquee;
---
+++ README.md
@@ -0,0 +1,59 @@
+#Marquee3G
+A Work in Progress™ but it's working. If you want to use it in its current state just do the following:
+
+**A Marquee's HTML must be structured like:**
+```html
+<div class="marquee">
+  <!-- You must have only a single container element, you can use any classname -->
+  <div class="marquee-container">
+    <!-- Whatever marquee contents you want -->
+  </div>
+</div>
+```
+
+**If you want to just initialise ALL `.marquee` or a custom selector:**
+
+```js
+import Marquee from '/path/to/Marquee3G';
+
+// Note that you must define "gsap" to which one youre using
+// either TimelineLite or TimelineMax
+Marquee.init({
+  selector: '.marquee', // default: .marquee
+  gsap: TimelineLite // required! you must specify which GSAP you're using
+});
+```
+
+**If you want more flexibility then you can also do your own init**
+
+```js
+import Marquee from '/path/to/Marquee3G';
+
+const marquees = document.querySelectorAll('.marquee');
+
+marquees.forEach(elem => {
+  const marqueeName = elem.dataset.name;
+  const marquee = new Marquee({
+    element: elem, // required! the DOMElement
+    gsap: TimelineLite, // required!
+    name: marqueeName // optional: the name of the marquee
+  });
+});
+
+console.log(Marquees);
+```
+
+**Marquee methods**
+All marquee instances are stored within a `window.Marquees` labelled by its `index` or a `custom name` if you have specified it in an instance's options when initialised.
+
+For example you have a Marquee named "footer-marquee" then you can just do the following methods when necessary:
+
+```js
+// Destroys the marquee and reverts back to original state
+Marquees['footer-marquee'].destroy(); 
+
+// Refreshes the marquee. For example, you change the font size/width of its contents
+Marquees['footer-marquee'].refresh(); 
+```
+
+That's all for now
